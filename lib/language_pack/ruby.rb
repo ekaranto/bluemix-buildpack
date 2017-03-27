b3bad861387ce8dfb88141b97a08176311497ab9
Evan Karantonis <ekaranto@gmail.com>
2017-03-27 16:55:40 +1100

    Changes to make the buildpack work with FUB on Bluemix

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -16,7 +16,7 @@ class LanguagePack::Ruby < LanguagePack::Base
   LIBYAML_PATH         = "libyaml-#{LIBYAML_VERSION}"
   BUNDLER_VERSION      = "1.7.12"
   BUNDLER_GEM_PATH     = "bundler-#{BUNDLER_VERSION}"
-  DEFAULT_RUBY_VERSION = "ruby-2.1.5"
+  DEFAULT_RUBY_VERSION = "ruby-2.2.2"
   RBX_BASE_URL         = "http://binaries.rubini.us/heroku"
   NODE_BP_PATH         = "vendor/node/bin"
 
@@ -624,10 +624,10 @@ ERROR
     instrument 'ruby.create_database_yml' do
       log("create_database_yml") do
         return unless File.directory?("config")
-        if File.exists?("config/database.yml")
-          topic("You already seem to have a config/database.yml. Using that instead of DATABASE_URL")
-          return
-        end
+        #if File.exists?("config/database.yml")
+        #  topic("You already seem to have a config/database.yml. Using that instead of DATABASE_URL")
+        #  return
+        #end
         topic("Writing config/database.yml to read from DATABASE_URL")
         File.open("config/database.yml", "w") do |file|
           file.puts <<-DATABASE_YML
